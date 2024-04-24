5e343b3d433f8e7fabd55e7daf4b13807ff4f038
linhtran <62147272+linhtranelectronics@users.noreply.github.com>
2024-04-24 23:23:18 +0700

    Update smart_bracelet.ino
    add sms

--- smart_bracelet.ino
+++ smart_bracelet.ino
@@ -1,31 +1,29 @@
+#include "defintion.h"
 
-
-#include "utilities.h"
-#include <JC_Button.h>
-//#include <GSM.h>
-
-#define TINY_GSM_DEBUG SerialMon
-#define SerialMon Serial
-#include <TinyGsmClient.h>
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, SerialMon);
-TinyGsm modem(debugger);
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
-Adafruit_MPU6050 mpu;
-
-#define SMS_TARGET "+84335644677"  //Change the number you want to send sms message
 const byte
   BUTTON_PIN(14),  // connect a button switch from this pin to ground
   LED_PIN(2);      // the standard Arduino "pin 13" LED
 
 Button myBtn(BUTTON_PIN);  // define the button
+
 int maxIndexSMS = 0;
+String phoneNumber1 = "";
+String phoneNumber2= "";
+String phoneNumber3 = "";
+String password = "123456";
+bool enableChange = false;
+uint32_t startChange = 0;
 
 sensors_event_t a, g, temp;
 float avgx, avgy, avgz;
 
+enum Action
+{
+  NONE,
+  PASSWORD,
+  LOCALATION
+}action;
+Action run;
 void setup() {
   Serial.begin(115200);
   myBtn.begin();
@@ -36,14 +34,27 @@ void setup() {
     delay(10);
   }
   pinMode(14, INPUT_PULLUP);
-    mpu.begin();
+  mpu.begin();
   //setupt motion detection
   mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
   mpu.setMotionDetectionThreshold(1);
   mpu.setMotionDetectionDuration(20);
-  mpu.setInterruptPinLatch(true);  // Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinLatch(true); 
   mpu.setInterruptPinPolarity(true);
   mpu.setMotionInterrupt(true);
+  // read phone number from Memory
+  savePhoneNumber.begin("PHONENUMBER", RO_MODE);
+  //phoneNumber1 = savePhoneNumber.getString("num1");
+  phoneNumber1 = "0335644677";
+  phoneNumber2 = savePhoneNumber.getString("num2");
+  phoneNumber3 = savePhoneNumber.getString("num3");
+  savePhoneNumber.end();
+
+  savePassword.begin("PASS", RO_MODE);
+  password = savePassword.getString("pass1");
+  Serial.println("pass " + password);
+  savePassword.end();
+  delAllSMS();
 
 }
 
@@ -51,9 +62,16 @@ void loop() {
 
   myBtn.read();
   readSensor(millis(), 100);
-
+  if (millis() > enableChange + 60000)
+  {
+    enableChange = false;
+    Serial.println("end change");
+    //modem.sendSMS(phoneNumber1, "sdt " + phoneNumber1+ "  "+ phoneNumber2 + "  "+ phoneNumber3 + 'end');
+  }
+  
  if (detectFall(7)) {
     Serial.println("detect");
+    //modem.sendSMS(phoneNumber1, "nguoi dung bi nga');
  }
 
   if (SerialAT.available()) {
@@ -65,6 +83,7 @@ void loop() {
       String num = s.substring(s.indexOf(",") + 1, s.indexOf("\n") - s.indexOf(","));
       maxIndexSMS = num.toInt();
       Serial.println(maxIndexSMS);
+      getContentSMS(maxIndexSMS);
     }
     else
     {
@@ -74,37 +93,116 @@ void loop() {
   if (Serial.available()) {
     SerialAT.write(Serial.read());
   }
-  delay(1);
   if (myBtn.wasReleased()) {
+    delAllSMS();
 
-    Serial.print("read sms    ");
-    Serial.print(maxIndexSMS);
-    Serial.print("    ");
-    String c = getContentSMS(maxIndexSMS);
-    Serial.println(c);
     //sendSMS();
   }
+
 }
-String getContentSMS(int index)
+
+void getContentSMS(int index)
 {
   String _buffer = readSMS(index);
-  uint8_t _idx1 = _buffer.indexOf("Pass ");
-  if (_idx1 != -1)
+  String fromNum = getNumberSMS(index);
+  if (_buffer.indexOf("Pass ") != -1)
   {
-    return _buffer.substring(_idx1+5, _idx1+11);
+    uint8_t _idx1 = _buffer.indexOf("Pass ");
+    if (_idx1 != -1)
+    {
+      String p1 = _buffer.substring(_idx1+5, _idx1+11);
+      String p2 = _buffer.substring(_idx1+12, _idx1+18);
+      String password = changePassword(p1, p2);
+      sendSMS(phoneNumber1, "pass: " + password);
+    }
+  }
+   if (_buffer.indexOf("Vitri") != -1)
+  {
+    Serial.print("from "+ fromNum + " myNum "+ phoneNumber1);
+    if (fromNum == phoneNumber1 || fromNum == phoneNumber2|| fromNum == phoneNumber3)
+    {
+      sendSMS(fromNum, "vi tri hien tai");
+    }
+  }
+  if (_buffer.indexOf("Change ") != -1)
+  {
+     uint8_t _idx1 = _buffer.indexOf("Change ");
+    String p1 = _buffer.substring(_idx1+7, _idx1+13);
+    if (p1 == password)
+    {
+      enableChange = true;
+      startChange = millis();
+      delay(100);
+      modem.sendSMS(fromNum, "sdt " + phoneNumber1+ "  "+ phoneNumber2 + "  "+ phoneNumber3);
+    }
+    else sendSMS(fromNum, "sai password");
+
+  }
+  if (_buffer.indexOf("Sdt1 ") != -1 && enableChange)
+  {
+    uint8_t _idx1 = _buffer.indexOf("Sdt1 ");
+    String p1 = _buffer.substring(_idx1+5, _idx1+15);
+    savePhoneNumber.begin("PHONENUMBER", RW_MODE);
+    savePhoneNumber.putString("num1", p1);
+    phoneNumber1 = savePhoneNumber.getString("num1");
+    Serial.println(phoneNumber1);
+    savePhoneNumber.end();
+    modem.sendSMS(fromNum, "sdt1 " + phoneNumber1);
+
+  }
+  if (_buffer.indexOf("Sdt2 ") != -1 && enableChange)
+  {
+    uint8_t _idx1 = _buffer.indexOf("Sdt2 ");
+    String p1 = _buffer.substring(_idx1+5, _idx1+15);
+    savePhoneNumber.begin("PHONENUMBER", RW_MODE);
+    savePhoneNumber.putString("num2", p1);
+    phoneNumber2 = savePhoneNumber.getString("num2");
+    Serial.println(phoneNumber2);
+    savePhoneNumber.end();
+    modem.sendSMS(fromNum, "sdt2 " + phoneNumber2);
     
+  }
+  if (_buffer.indexOf("Sdt3 ") != -1 && enableChange)
+  {
+    uint8_t _idx1 = _buffer.indexOf("Sdt3 ");
+    String p1 = _buffer.substring(_idx1+5, _idx1+15);
+    savePhoneNumber.begin("PHONENUMBER", RW_MODE);
+    savePhoneNumber.putString("num3", p1);
+    phoneNumber1 = savePhoneNumber.getString("num3");
+    Serial.println(phoneNumber3);
+    savePhoneNumber.end();
+    modem.sendSMS(fromNum, "sdt3 " + phoneNumber3);
+    
+  }
+
+}
+String changePassword(String oldPass, String newPass)
+{
+  if(oldPass == password)
+  {
+    savePassword.begin("PASS", RW_MODE);
+    savePassword.putString("pass1", newPass);
+    String p = savePassword.getString("pass1");
+    savePassword.end();
+    return p;
+  }
+  return password;
+}
+void unEnableChange(uint16_t after)
+{
+  if (millis() > startChange + after)
+  {
+    enableChange = false;
   }
   
 }
-
 bool receiveSMS() {
 }
-void sendSMS() {
-  Serial.print("Init success, start to send message to  ");
-  Serial.println(SMS_TARGET);
+void sendSMS(String num, String content) {
+  //Serial.print("Init success, start to send message to  ");
+  //Serial.println(num);
 
-  String imei = modem.getIMEI();
-  bool res = modem.sendSMS(SMS_TARGET, String("Hello from ") + imei);
+  bool res = modem.sendSMS(num, content);
 
   Serial.print("Send sms message ");
   Serial.println(res ? "OK" : "fail");
@@ -144,13 +242,13 @@ String getNumberSMS(uint8_t index) {
   {
     uint8_t _idx1 = _buffer.indexOf("+CMGR:");
     _idx1 = _buffer.indexOf("\",\"", _idx1 + 1);
-    return _buffer.substring(_idx1 + 3, _buffer.indexOf("\",\"", _idx1 + 4));
+    return "0" + _buffer.substring(_idx1 + 3, _buffer.indexOf("\",\"", _idx1 + 4)).substring(3, 12);
   } else {
     return "";
   }
 }
 bool delAllSMS() {
-  SerialAT.print(F("at+cmgda=1,1\n\r"));
+  SerialAT.print(F("at+cmgd=1\n\r"));
   String _buffer = _readSerial();
   if (_buffer.indexOf("OK") != -1) {
     maxIndexSMS = 0;
